52a21636a7e001d00793f3b8a1da9480f598123d
Karol <karol144y@gmail.com>
2025-03-27 23:27:00 +0100

    w pliku MainActivity.kt pobieram wartosci z ankiety i dodaje funkcje ktora zmienia obrazki

--- app/src/main/java/com/example/kawiarnia1/MainActivity.kt
+++ app/src/main/java/com/example/kawiarnia1/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.kawiarnia1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.SeekBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +20,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        val checkBoxMleko = findViewById<CheckBox>(R.id.checkBoxMleko)
+        val checkBoxCukier = findViewById<CheckBox>(R.id.checkBoxCukier)
+        val seekBar = findViewById<SeekBar>(R.id.ilosc)
+        val button = findViewById<Button>(R.id.ZlozZamowienie)
+        val imageView = findViewById<ImageView>(R.id.Obrazek)
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.radioEspresso -> imageView.setImageResource(R.drawable.espresso)
+                R.id.radioCappuccino -> imageView.setImageResource(R.drawable.capuccino)
+                R.id.radioLatte -> imageView.setImageResource(R.drawable.latte)
+                else -> {}
+            }
         }
     }
 }
